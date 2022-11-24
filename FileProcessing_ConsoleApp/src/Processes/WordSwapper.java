95f78f76217a925e27be12faafeb92d123437eff
Vesislav Dimitrov <91593275+vesislavdimitrov@users.noreply.github.com>
2022-11-24 13:52:12 +0200

    Update WordSwapper.java

--- FileProcessing_ConsoleApp/src/Processes/WordSwapper.java
+++ FileProcessing_ConsoleApp/src/Processes/WordSwapper.java
@@ -1,29 +1,32 @@
 package Processes;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class WordSwapper {
-    
-    public  void swapWords(ArrayList<String> fileLines, String filePath, Scanner scan)  {
+    public  void swapWords(ArrayList<ArrayList<String>> fileData, String filePath, Scanner scan) throws IOException {
 
         Writer writer = new Writer();
-        int firstWordX,firstWordY,secondWordX,secondWordY;
-        String[] firstWords;
-        String[] secondWords;
+        int firstWordY,firstWordX ,secondWordY ,secondWordX ;
+
+        int innerSize = 0;
+        for(ArrayList<String> child:fileData){
+            innerSize = child.size();
+        }
 
         System.out.println("Enter the line (Y) and position (X) of the first word: ");
         while (true){
             firstWordY = scan.nextInt()-1;
-            if(firstWordY>=0 && firstWordY<fileLines.size()){
-                firstWords = fileLines.get(firstWordY).split(" ");
+            if(firstWordY>=0 && firstWordY<fileData.size()){
                 break;
             }
             System.err.println("Enter a valid line!");
         }
         while (true){
             firstWordX = scan.nextInt()-1;
-            if(firstWordX>=0 && firstWordX < firstWords.length){
+            if(firstWordX>=0 && firstWordX < innerSize){
                 break;
             }
             System.err.println("Enter a valid position!");
@@ -31,34 +34,24 @@ public class WordSwapper {
         System.out.println("Enter the line (Y) and position (X) of the second word: ");
         while (true){
             secondWordY = scan.nextInt()-1;
-            if(secondWordY>=0 && secondWordY<fileLines.size()){
-                secondWords = fileLines.get(secondWordY).split(" ");
+            if(secondWordY>=0 && secondWordY<fileData.size()){
                 break;
             }
             System.err.println("Enter a valid line!");
         }
         while (true){
             secondWordX = scan.nextInt()-1;
-            if(secondWordX>=0 && secondWordX <secondWords.length){
+            if(secondWordX>=0 && secondWordX < innerSize){
                 break;
             }
             System.err.println("Enter a valid position!");
         }
 
-        if (firstWordY != secondWordY) {
-            firstWords[firstWordX] += secondWords[secondWordX];
-            secondWords[secondWordX] = firstWords[firstWordX].substring(0, firstWords[firstWordX].length() - secondWords[secondWordX].length());
-            firstWords[firstWordX] = firstWords[firstWordX].substring(secondWords[secondWordX].length());
-            fileLines.set(firstWordY, String.join(" ", firstWords));
-            fileLines.set(secondWordY, String.join(" ", secondWords));
-        }
-        else{
-            String temp = firstWords[firstWordX];
-            firstWords[firstWordX] = firstWords[secondWordX];
-            firstWords[secondWordX] = temp;
-            fileLines.set(firstWordY, String.join(" ", firstWords));
-        }
+        String temp = fileData.get(firstWordY).get(firstWordX);
+        fileData.get(firstWordY).set(firstWordX, fileData.get(secondWordY).get(secondWordX));
+        fileData.get(secondWordY).set(secondWordX, temp);
+
+        writer.writeToFile(fileData,filePath);
         System.out.println("Successfully swapped words!");
-        writer.writeToFile(fileLines,filePath);
     }
 }
